9ebac3faaef7493d1fea8fe436965ad709a182b5
ANUPAMA S NAIR <120626887+anupamaSN26@users.noreply.github.com>
2023-04-18 12:05:33 +0530

    Add files via upload

---
+++ default cons/A.java
@@ -0,0 +1,13 @@
+class A{
+    private int data=50;
+    private void msg(){
+        System.out.println("Hello JAVA...!!!");
+    }
+    public class Simple{
+        public static void main(String args[]){
+            A obj=new A();
+            System.out.prinltln(obj.data);
+            obj.msg();
+        }
+    }
+}
